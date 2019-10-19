e3cdb497800127f48c58d54b81694c7a60979f60
samrod826 <samrodriguez826@gmail.com>
2019-10-19 09:35:01 -0400

    displays per button clicks

--- api.js
+++ api.js
@@ -3,32 +3,25 @@ var topics = ["horror","anime","baseball","movies","math","skate","art","fashion
 for(i = 0; i < topics.length; i++){
     var buttons = $("<button>").text(topics[i])
     buttons.attr("id", topics[i])
+    buttons.addClass("gifs")
     $("#button").append(buttons)
-    console.log(topics[i])
 }
-
-$("#button").on("click",function(){
-    
-})
-
-var idea = "funny"
-var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=9EHeN39gBFXMES8bye8aCt2SDxTc9wE5&q=" + idea + "&limit=10"
-
 function showImg(obj) {
     $("#gifs").append($("<img>").attr("src", obj.images.downsized.url));
 }
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
-    console.log(response);
-    response.data.forEach(showImg);
-});
-
-var sample = function(){
-   var a = b = 3;
+function displayGif(){
+    $("#gifs").empty();
+    var topic = $(this).attr("id");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=9EHeN39gBFXMES8bye8aCt2SDxTc9wE5&q=" + topic + "&limit=10"
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+        response.data.forEach(showImg);
+    });
 }
-sample();
-console.log("Is a defined?", typeof a !== "undefined");
-console.log("Is b defined?", typeof b !== "undefined");
+
+$(document).on("click", ".gifs", displayGif);
