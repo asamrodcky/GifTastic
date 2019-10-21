e99d34be26accc28b5225aafabb487777b825477
Sam Rodriguez <srodriguez@srodriguez-mac.local>
2019-10-21 16:09:09 -0400

    working on play/pause

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -10,10 +10,6 @@ function renderButtons(){
     }
 }
 
-function showImg(obj) {
-    $("#gifs").append($("<img>").attr("src", obj.images.downsized.url));
-}
-
 function displayGif(){
     var topic = $(this).attr("id");
     var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=9EHeN39gBFXMES8bye8aCt2SDxTc9wE5&q=" + topic + "&limit=10"
@@ -22,12 +18,11 @@ function displayGif(){
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-        console.log(response);
+        // console.log(response);
         var results = response.data;
 
         for (var i = 0; i < results.length; i++) {
             var gifDiv = $("<div>");
-            console.log(gifDiv);
             gifDiv.attr("id","gif");
 
             var rating = results[i].rating;
@@ -35,8 +30,11 @@ function displayGif(){
             var p = $("<p>").text("Rating: " + rating);
 
             var gifImage = $("<img>");
-            gifImage.attr("src", results[i].images.fixed_height.url);
-            gifImage.attr("data-state","animate")
+            gifImage.attr("src", results[i].images.fixed_height_still.url);
+            gifImage.attr("data-still", results[i].images.fixed_height_still.url);
+            gifImage.attr("data-animate", results[i].images.fixed_height.url);
+            gifImage.attr("data-state","still");
+            gifImage.addClass("gif")
 
             gifDiv.prepend(p);
             gifDiv.prepend(gifImage);
@@ -56,10 +54,21 @@ $("#add-button").on("click", function(event) {
     renderButtons();
 
     $("#topic-input").val("")
-  });
+});
+
+$(".gif").on("click", function(){
+    var state = $(this).attr("data-state")
+    console.log(state)
+    if(state == "still"){
+        console.log("true")
+    }
+    // else{
+
+    // }
+})
 
 renderButtons();
-$(document).on("click", ".gifs", displayGif);
+$(document).on("click", ".topics", displayGif);
 
 // Psuedocode:
 
